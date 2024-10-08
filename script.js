22c25e4cc38eb861ade5a9f98fd37d25b4a2e23b
mast8 <ronald.miranda.rodriguez@gmail.com>
2024-10-08 13:04:55 -0400

    min and avg functions

--- script.js
+++ script.js
@@ -7,23 +7,38 @@ enter_btn.addEventListener("click", function () {
 
 
 // input 234, 1321,5345,3424,34324 
+234, 1321,5345,3424,34324 ; 234, 1321,5345,3424,34324 
 // or 123 231 424 234
 
 
 function getInput( ){ 
   let data = document.getElementById("data").value;
   let type = document.getElementById("graph").value;
-  dataArr = data.split(",");
-  console.log(dataArr);
-  findmax(dataArr)
+  dataArr = data.split(",").map(Number);
+  find_max(dataArr);
+  find_min(dataArr);
+  find_average(dataArr);
   graph(dataArr,type);
 
 }
 
-function findmax(dataArr){
-  //let max = document.getElementById("max");
-  higest = Math.max(...dataArr);
-  document.getElementById("max").innerHTML = `max is `+ higest;
+function find_max(dataArr){
+  higest = Math.max(...dataArr); 
+  document.getElementById("max").innerHTML = `Max is `+ higest;
+}
+
+function find_min(dataArr){
+  lowest = Math.min(...dataArr); 
+  document.getElementById("min").innerHTML = `Min is `+ lowest;
+}
+
+function find_average(dataArr) {
+  var total = 0;
+  for(var i = 0; i < dataArr.length; i++) {
+      total += dataArr[i];
+  }
+  var avg = total / dataArr.length;
+  document.getElementById("avg").innerHTML = `Avg is `+ avg;
 }
 
 //charts
